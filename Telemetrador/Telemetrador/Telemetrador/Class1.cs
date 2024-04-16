0998193ec2a33e306351d51cda2d2817fc3be572
Sebaaaaas <sebsan01@ucm.es>
2024-04-17 00:46:00 +0200

    Cambio a dll de C#

---
+++ Telemetrador/Telemetrador/Telemetrador/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Telemetrador
+{
+    public class Class1
+    {
+        public int c;
+
+        public void AddValues(int a, int b)
+        {
+            c = a + b;
+        }
+
+        public static int GenerateRandom(int min, int max)
+        {
+            System.Random rand = new System.Random();
+            return rand.Next(min, max);
+        }
+    }
+}
--- Telemetrador/Telemetrador/evento.cpp
+++
@@ -1,1 +0,0 @@
-#include "evento.h"
