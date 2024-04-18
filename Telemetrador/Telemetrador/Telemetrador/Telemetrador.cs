615cc45ba1c946f750cf233ecf293414458cfe29
Sebaaaaas <sebsan01@ucm.es>
2024-04-18 16:04:24 +0200

    Telemetrador

---
+++ Telemetrador/Telemetrador/Telemetrador/Telemetrador.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telemetrador
+{
+    enum Events { startGame, endGame};
+    public class Telemetrador
+    {
+        private Queue<Events> events;
+        private static Telemetrador instance = null;
+
+        private Telemetrador()
+        {
+            events = new Queue<Events>();
+        }
+        public Telemetrador Instance()
+        {
+            return instance;
+        }
+
+        public static bool Inicializacion()
+        {
+            if (instance != null) return false;
+
+            instance = new Telemetrador();
+            return true;
+        }
+    }
+}
