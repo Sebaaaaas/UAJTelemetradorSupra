ce2dbcb852912ec035f93530ef5c16ec804b37f0
GonzaloFdezMoreno <gonzaf05@ucm.es>
2024-04-26 18:54:07 +0200

    pilla el tiempo restante

--- Telemetrador/Telemetrador/Telemetrador/PLayerBreath.cs
+++ Telemetrador/Telemetrador/Telemetrador/PLayerBreath.cs
@@ -8,11 +8,12 @@ namespace TelemetradorNamespace
 {
     public class PLayerBreath : Event
     {
-        public PLayerBreath(float timestamp)
+        public PLayerBreath(float timestamp,float min,float sec, float mill)
         {
             setEventType(EventType.playerBreathes);
             data.Add("name", "Respira");
             data.Add("timestamp", timestamp.ToString());
+            data.Add("reamainingTime",min.ToString()+" "+ sec.ToString()+" "+ mill.ToString());
         }
 
     }
