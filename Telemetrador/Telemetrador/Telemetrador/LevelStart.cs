677414a90186ab66aee740b1f8f7f3799c340e97
Sebaaaaas <sebsan01@ucm.es>
2024-04-26 20:02:01 +0200

    Level start

---
+++ Telemetrador/Telemetrador/Telemetrador/LevelStart.cs
@@ -0,0 +1,13 @@
+﻿
+namespace TelemetradorNamespace
+{
+    public class LevelStart : Event
+    {
+        public LevelStart(float timestamp)
+        {
+            setEventType(EventType.startGame);
+            data.Add("name", "LevelStart");
+            data.Add("timestamp", timestamp.ToString());
+        }
+    }
+}
